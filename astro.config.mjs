a6af1f698f2d9283b8dbe22bc174971712141b9e
Dominic Baca <dbaca04@gmail.com>
2025-05-18 01:05:32 -0600

    Improve content readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,4 +22,17 @@ export default defineConfig({
     experimentalObjectFit: 'cover', // Default object-fit style
     experimentalObjectPosition: 'center', // Default object-position style
   },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'github-dark',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
